a9e165fbff912a1d4be0d59fc57e168ba5f97d9e
HsiangDX <eric4042440424@gmail.com>
2024-02-27 21:55:44 +0800

    [Debug] Right Version
    Signed-off-by: HsiangDX <eric4042440424@gmail.com>

--- hw3.cc
+++ hw3.cc
@@ -11,8 +11,6 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <iostream>
-#include <chrono>
 
 #define vMax 6000
 #define wMax 1000
@@ -21,15 +19,12 @@
 int vertexNum, edgeNum;
 int numThread;
 int graph[vMax][vMax];
-int output1[2];
-int output2[6000][6000];
 int *input_bin,*output_bin;
 pthread_barrier_t barrier;
 
 void* APSP(void* args)
 {
     int *data = (int*)args;
-    //int vertex_temp = (vertexNum/4) * 4;
     for(int i=*data;i<vertexNum;i+=numThread){
         for(int j=0;j<vertexNum;j++){
             graph[i][j] = INF;
@@ -51,20 +46,10 @@ void* APSP(void* args)
     }
 
     pthread_barrier_wait(&barrier);
+
+    pthread_barrier_wait(&barrier);
     for(int k=0;k<vertexNum;k++){
         for(int i=*data;i<vertexNum;i+=numThread){
-            /*for(int j=0;j<vertex_temp;j+=4){
-                __m128i as = _mm_lddqu_si128((__m128i const*)&graph[k][j]);
-                __m128i bs = _mm_set1_epi32(graph[i][k]);
-                __m128i cs = _mm_lddqu_si128((__m128i const*)&graph[i][j]);
-                __m128i temp_vec = _mm_add_epi32(bs,as);
-                cs = _mm_min_epi32(temp_vec,cs);
-                _mm_store_si128((__m128i*)&graph[i][j],cs);
-                //int temp = graph[i][k]+graph[k][j];
-                //if(temp<graph[i][j]){
-                //    graph[i][j]=temp;
-                //}
-            }*/
             for(int j=0;j<vertexNum;j++){
                 int temp = graph[i][k]+graph[k][j];
                 if(temp<graph[i][j]){
@@ -94,7 +79,7 @@ void* APSP(void* args)
     pthread_exit(NULL);
 }*/
 
-/*void Floydwarshall()
+/*void floydwarshall()
 {
     for(int k=0;k<vertexNum;k++){
         #pragma omp parallel for num_threads(numThread) schedule(dynamic) collapse(2)
@@ -123,61 +108,91 @@ void* APSP(void* args)
 
 int main(int argc,char** argv)
 {
-    std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
     cpu_set_t cpu_set;
     sched_getaffinity(0, sizeof(cpu_set), &cpu_set);
     printf("%d cpus available\n", CPU_COUNT(&cpu_set));
     numThread = CPU_COUNT(&cpu_set);
+    //char *inputFile = argv[1];
+    //char *outputFile = argv[2];
+    //std::ifstream f(argv[1]);
+    std::ofstream fout(argv[2], std::ios::out | std::ios::binary); 
 
-    std::ofstream fout("testcase.in", std::ios::out | std::ios::binary); 
-    output1[0] = 6000;
-    output1[1] = 5999*6000;
+    //int vertexNum, edgeNum;
+    /*f.seekg (0, std::ios_base::beg);
+    f.read((char*)&vertexNum,sizeof(vertexNum));
+    f.read((char*)&edgeNum,sizeof(edgeNum));*/
+    //printf("(%d,%d)\n",vertexNum,edgeNum);
 
-    for(int i=0;i<6000;i++){
-        for(int j=0;j<6000;j++){
-            if(i!=j){
-                output2[i][j] = 1000 - 1000 * i /6000;
-            }
+    int fd = open(argv[1], O_RDONLY);
+    input_bin = (int*)mmap(NULL, 2 * sizeof(int), PROT_READ, MAP_SHARED, fd, 0);
+    vertexNum = input_bin[0];
+    edgeNum = input_bin[1];
+    input_bin = (int*)mmap(NULL, (edgeNum * 3 + 2) * sizeof(int), PROT_READ, MAP_SHARED, fd, 0);
+
+    //#pragma omp parallel for num_threads(numThread) collapse(2)
+    /*for(int i=0;i<vertexNum;i++){
+        for(int j=0;j<vertexNum;j++){
+            graph[i][j] = INF;
         }
+    }*/
+    //#pragma omp parallel for num_threads(numThread)
+    /*for(int i=0;i<vertexNum;i++){
+        graph[i][i] = 0;
+    }*/
+
+    /*for(int i=0;i<edgeNum;i++){
+        //int e[3];
+        //f.read((char*)e, sizeof(e));
+        //printf("%4d -> %4d, dist=%4d\n", e[0], e[1], e[2]);
+        //graph[e[0]][e[1]] = e[2];
+        graph[input_bin[2+i*3]][input_bin[3+i*3]] = input_bin[4+i*3];
+    }*/
+
+    pthread_barrier_init(&barrier,NULL,numThread);
+    pthread_t threads[numThread];
+    int tids[numThread];
+    for(int i=0;i<numThread;i++){
+        tids[i] = i;
+        pthread_create(&threads[i],NULL,APSP,(void*)&tids[i]);
     }
 
-    fout.write((char*)&output1[0],sizeof(int));
-    fout.write((char*)&output1[1],sizeof(int));
-
-    for(int i=0;i<6000;i++){
-        for(int j=0;j<6000;j++){
-            if(i!=j){
-                fout.write((char*)new int(i),sizeof(int));
-                fout.write((char*)new int(j),sizeof(int));
-                fout.write((char*)&output2[i][j],sizeof(int));
-            }
-        }
+    for(int i=0;i<numThread;i++){
+        pthread_join(threads[i],NULL);
     }
+    //floydwarshall();
+    //std::thread threads[numThread];
+    /*std::vector<std::thread> threads; 
+    for(int k=0;k<vertexNum;k++){
+        for(int i=0;i<numThread;i++){
+            //threads[i](APSP_CPP,k,i);
+            threads.push_back(std::thread(APSP_CPP,k,i));
+        }
+        for(int j=0;j<numThread;j++){
+            threads[j].join();
+        }
+    }*/
+    //munmap(input_bin, (edgeNum * 3 + 2) * sizeof(int));
+    /*int fd2 = open(argv[2],O_RDWR|O_CREAT|O_TRUNC);
+    int result = lseek(fd2, sizeof(int)*vertexNum*vertexNum-1, SEEK_SET);
+    result = write(fd2, "", 1);
+    output_bin = (int*)mmap(0, sizeof(int)*vertexNum*vertexNum, PROT_READ|PROT_WRITE, MAP_SHARED, fd2, 0);
+    for(int i=0;i<vertexNum;i++){
+        for(int j=0;j<vertexNum;j++){
+            output_bin[i*vertexNum+j] = graph[i][j];
+        }
+    }*/
+    //close(fd2);
 
-    // Cal time for report
-    /*std::chrono::steady_clock::time_point tout1 = std::chrono::steady_clock::now();
+    /*for(int i=0;i<vertexNum;i++){
+        for(int j=0;j<vertexNum;j++){
+            fout.write((char*)&graph[i][j],sizeof(int));
+        }
+    }*/
+
     for(int i=0;i<vertexNum;i++){
         fout.write((char*)&graph[i],sizeof(int)*vertexNum);
     }
-    std::chrono::steady_clock::time_point tout2 = std::chrono::steady_clock::now();
 
-    std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-    std::cout << "ALL time: "
-    << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()
-    << "ms.\n";
-
-    std::cout << "In time: "
-    << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() - std::chrono::duration_cast<std::chrono::milliseconds>(tcmp2 - tcmp1).count() - std::chrono::duration_cast<std::chrono::milliseconds>(tout2 - tout1).count()
-    << "ms.\n";
-
-    std::cout << "cmp time: "
-    << std::chrono::duration_cast<std::chrono::milliseconds>(tcmp2 - tcmp1).count()
-    << "ms.\n";
-
-    std::cout << "Out time: "
-    << std::chrono::duration_cast<std::chrono::milliseconds>(tout2 - tout1).count()
-    << "ms.\n";*/
-
-    f.close();
-    fout.close();
+    //f.close();
+    //fout.close();
 }
